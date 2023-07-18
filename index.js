4f17b3dc599f69f63bf7917d0588bd26acdb8b0d
R-Joshua-Sujith <joshuasujith14@gmail.com>
2023-07-18 18:19:57 +0530

    completed

---
+++ index.js
@@ -0,0 +1,125 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const Mentor = require('./models/mentor');
+const Student = require('./models/student');
+
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful"))
+  .catch((err) => console.log(err));
+
+app.get("/", async (req, res) => {
+  res.send("Day 41 Backend")
+})
+
+app.post('/mentors', async (req, res) => {
+  try {
+    const mentor = new Mentor(req.body);
+    await mentor.save();
+    res.status(201).json(mentor);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.post('/students', async (req, res) => {
+  try {
+    const student = new Student(req.body);
+    await student.save();
+    res.status(201).json(student);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.put('/mentors/:mentorId/students', async (req, res) => {
+  const { mentorId } = req.params;
+  const { studentIds } = req.body;
+
+  try {
+    const mentor = await Mentor.findById(mentorId);
+    if (!mentor) {
+      return res.status(404).json({ error: 'Mentor not found' });
+    }
+
+    const students = await Student.find({ _id: { $in: studentIds } });
+    if (students.length !== studentIds.length) {
+      return res.status(404).json({ error: 'One or more students not found' });
+    }
+
+    mentor.students = studentIds;
+    await mentor.save();
+
+    res.json(mentor);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put('/students/:studentId/mentor', async (req, res) => {
+  const { studentId } = req.params;
+  const { mentorId } = req.body;
+
+  try {
+    const student = await Student.findById(studentId);
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+
+    const mentor = await Mentor.findById(mentorId);
+    if (!mentor) {
+      return res.status(404).json({ error: 'Mentor not found' });
+    }
+
+    student.mentor = mentorId;
+    await student.save();
+
+    res.json(student);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/mentors/:mentorId/students', async (req, res) => {
+  const { mentorId } = req.params;
+
+  try {
+    const mentor = await Mentor.findById(mentorId).populate('students');
+    if (!mentor) {
+      return res.status(404).json({ error: 'Mentor not found' });
+    }
+
+    res.json(mentor.students);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.get('/students/:studentId/mentor', async (req, res) => {
+  const { studentId } = req.params;
+
+  try {
+    const student = await Student.findById(studentId).populate('mentor');
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+
+    const mentor = student.mentor;
+
+    res.json(mentor);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Backend Server is running");
+})
